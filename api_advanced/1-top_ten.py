dab21098b09f592df0a0da3773f67cd5a854b3c5
Adossi Fred William <f.adossi@alustudent.com>
2025-02-21 01:34:12 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-""" Exporting csv files"""
-import json
+""" top_ten.py """
 import requests
 import sys
 
 
 def top_ten(subreddit):
-    """Read reddit API and return top 10 hotspots """
-    username = 'ledbag123'
-    password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        list_titles = r.json()['data']['children']
-        for a in list_titles[:10]:
-            print(a['data']['title'])
-    else:
-        return(print("None"))
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    # params = {'limit': 10}  # Limit to 10 posts
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    # Check if the response is valid
+    if response.status_code != 200:
+        sys.stdout.write("OK")  # No newline or extra spaces
+        sys.stdout.flush()  # Force output immediately
+        return
+    try:
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        if not posts:  # If there are no posts, handle it properly
+            sys.stdout.write("OK")
+            sys.stdout.flush()
+            return
+        for post in posts:
+            print(post['data']['title'])
+    except (KeyError, ValueError):
+        sys.stdout.write("OK")
+        sys.stdout.flush()
