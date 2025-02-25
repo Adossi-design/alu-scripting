82af81d0da3989b1e14276f9ab1b54ba827c5e8a
Adossi Fred William <f.adossi@alustudent.com>
2025-02-25 19:50:08 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""DOCS"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    # params = {'limit': 10}  # Limit to 10 posts
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    # Check if the response is valid
-    if response.status_code != 200:
-        sys.stdout.write("OK")  # No newline or extra spaces
-        sys.stdout.flush()  # Force output immediately
-        return
-    try:
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-        if not posts:  # If there are no posts, handle it properly
-            sys.stdout.write("OK")
-            sys.stdout.flush()
-            return
-        for post in posts:
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
             print(post['data']['title'])
-    except (KeyError, ValueError):
-        sys.stdout.write("OK")
-        sys.stdout.flush()
+    else:
+        print(None)
